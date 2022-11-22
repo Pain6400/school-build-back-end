5e418d8f38ef5642000e4ebe54b0f145251662cd
Pain6400 <45575989+Pain6400@users.noreply.github.com>
2022-11-21 22:35:13 -0600

    crud

--- controllers/role.controller.js
+++ controllers/role.controller.js
@@ -34,12 +34,36 @@ export const usuarioRoles = async(req, res) => {
     }
 }
 
+export const usuarioRolesByUserId = async(req, res) => {
+    try {
+        const { userId } = req.params;
+        const roles = await IdentityUserRoles.find({user_id: userId});
+
+        return res.status(200).json({ status: true, message: "Peticion Exitosa", roles}); 
+    } catch (error) {
+        return res.status(500).json({ status: false, message: error.message})
+    }
+}
+
 export const createUserRole = async(req, res) => {
     try {
         const { user_id, role_id } = req.body;
         const userRole = new IdentityUserRoles({ user_id, role_id });
         await userRole.save();
         return res.status(201).json({ status: true, message: "Role asignado a usuario correctamente", userRole })
+    } catch (error) {
+        return res.status(500).json({ status: false, message: error.message})
+    }
+}
+
+export const deleteUserRole = async(req, res) => {
+    try {
+        const { userRolId } = req.params;
+        const userRole = IdentityUserRoles.findById(userRolId);
+        if(!userRole) return res.status(403).json({ status: false, message: "El rol no existe" }); 
+
+        await userRole.remove();
+        return res.status(201).json({ status: true, message: "Role eliminado correctamente" })
     } catch (error) {
         return res.status(500).json({ status: false, message: error.message})
     }
--- routes/role.router.js
+++ routes/role.router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createRole, createUserRole, getRoles, usuarioRoles } from "../controllers/role.controller.js";
+import { createRole, createUserRole, deleteUserRole, getRoles, usuarioRoles, usuarioRolesByUserId } from "../controllers/role.controller.js";
 import { checkRoleAuth } from "../middlewares/checkRoleAuth.js";
 import { requireToken } from "../middlewares/requireToken.js";
 import { bodyRolValidator, bodyUserRolValidator } from "../middlewares/validatorManager.js";
@@ -7,8 +7,9 @@ import { bodyRolValidator, bodyUserRolValidator } from "../middlewares/validator
 const router = Router();
 
 router.get("/", requireToken, checkRoleAuth(["super_admin"]), getRoles);
+router.get("/usuarioRoles", requireToken, checkRoleAuth(["super_admin", "admin"]), usuarioRoles)
+router.get("/rolesByUserId/:userId", requireToken, checkRoleAuth(["super_admin", "admin"]), usuarioRolesByUserId);
 router.post("/Create", requireToken, checkRoleAuth(["super_admin"]), bodyRolValidator, createRole);
-router.get("/usuarioRoles", requireToken, checkRoleAuth(["super_admin"]), usuarioRoles)
 router.post("/createUserRole", requireToken, checkRoleAuth(["super_admin"]), bodyUserRolValidator, createUserRole);
-
+router.delete("/deleteUserRoleById/:userRolId", requireToken, checkRoleAuth(["super_admin", "admin"]), bodyUserRolValidator, deleteUserRole);
 export default router;
