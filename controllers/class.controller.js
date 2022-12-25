edaf63e6acf674f49d9868e78ce3e257a118b9c6
Pain6400 <45575989+Pain6400@users.noreply.github.com>
2022-12-25 12:56:25 -0600

    obtener clases

--- controllers/class.controller.js
+++ controllers/class.controller.js
@@ -1,5 +1,6 @@
 import { Class } from "../models/Class.js"
 import { Class_Room } from "../models/Class_Room.js";
+import { Student_Class } from "../models/Student_Class.js";
 
 export const getClassRooms = async(req, res) => {
     try {
@@ -41,6 +42,20 @@ export const createClass = async(req, res) => {
          
         await classObject.save();
         return res.status(200).json({ status: true, message: "Clase creada correctamente", classObject});        
+    } catch (error) {
+        return res.status(500).json({ status: false, message: error.message})
+    }
+}
+
+export const getClassByStudent = async(req, res) => {
+    try {
+        const { student_id } = req.params;
+        const classes = await Student_Class
+                                .find({ student_id: student_id })
+                                .populate({ path: "class_id", model: "Class", select: { school_id: 1, class_room_id: 1, teacher_id: 1, grade_id: 1, name: 1 }})
+                                .select({ class_id: 1 });
+        
+        return res.status(200).json({ status: true, message: "Peticion Exitosa", classes});        
     } catch (error) {
         return res.status(500).json({ status: false, message: error.message})
     }
--- routes/class.router.js
+++ routes/class.router.js
@@ -3,7 +3,7 @@ import { checkRoleAuth } from "../middlewares/checkRoleAuth.js";
 import { requireToken } from "../middlewares/requireToken.js";
 import { bodyClassRoomValidator, bodyClassValidator } from "../middlewares/validatorManager.js";
 import { ROLES_SETTING } from "../config/roles_setting.js";
-import { createClass, createClassRoom, getClassByScool, getClassRooms } from "../controllers/class.controller.js";
+import { createClass, createClassRoom, getClassByScool, getClassByStudent, getClassRooms } from "../controllers/class.controller.js";
 
 const router = Router();
 
@@ -11,5 +11,6 @@ router.get("/getClassRooms", requireToken, checkRoleAuth([ROLES_SETTING.SUPERADM
 router.post("/createClassRoom", requireToken, checkRoleAuth([ROLES_SETTING.SUPERADMIN, ROLES_SETTING.ADMIN]),bodyClassRoomValidator, createClassRoom);
 router.get("/getClassByScool/:school_id", requireToken, checkRoleAuth([ROLES_SETTING.SUPERADMIN, ROLES_SETTING.ADMIN]), getClassByScool);
 router.post("/createClass", requireToken, checkRoleAuth([ROLES_SETTING.SUPERADMIN, ROLES_SETTING.ADMIN]), bodyClassValidator, createClass);
+router.get("/getClassByStudent/:student_id", requireToken, checkRoleAuth([ROLES_SETTING.SUPERADMIN, ROLES_SETTING.STUDENT]), getClassByStudent);
 
 export default router;
