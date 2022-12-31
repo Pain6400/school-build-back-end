43f8dea28b5e128f2f7dc3321a35f5ecbc9ae48a
Pain6400 <45575989+Pain6400@users.noreply.github.com>
2022-12-30 21:39:37 -0600

    seguridad

--- utils/tokenManager.js
+++ utils/tokenManager.js
@@ -19,6 +19,7 @@ export const generateRefreshToken = (uid, res) => {
         res.cookie("refreshToken", refreshToken, {
             httpOnly: true,
             secure: !(process.env.MODO === "developer"),
+            sameSite: 'None',
             expires: new Date(Date.now() + expiresIn * 1000)
         });
         
